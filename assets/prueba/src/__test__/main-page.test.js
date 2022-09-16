6788337447270e19266259303d757b7481652bfa
Maximiliano Cabrera <maximilianocabrerap@gmail.com>
2022-09-16 17:38:37 -0300

    Fix
    Resuelvo conflicto en localhost y test

--- assets/prueba/src/__test__/main-page.test.js
+++ assets/prueba/src/__test__/main-page.test.js
@@ -1,7 +1,28 @@
-import React from "react";
-import { render, screen } from '@testing-library/react';
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import { rest } from "msw"
+import { setupServer } from "msw/node"
 
-import MainPage from "../components/main-page";
+import MainPage from "../components/main-page"
+
+const fakeQuotes = [
+    { quote: "Gah, stupid sexy Flanders!" },
+    { quote: "Eat my shorts" },
+    { quote: "Shut up, brain. I got friends now. I don't need you anymore" },
+]
+
+const server = setupServer(
+    rest.get("/quotes", (req, res, ctx) => {
+        return res(ctx.json(fakeQuotes))
+    })
+)
+
+// Enable API mocking before tests.
+beforeAll(() => server.listen())
+
+// Disable API mocking after the tests are done.
+afterAll(() => server.close())
+
 
 beforeEach(() => render(<MainPage />))
 
@@ -18,8 +39,12 @@ describe("Quotes List", () => {
 })
 
 it("must contain quote value", async () => {
-    const [firstQuote, secondQuote, thirdQuote] = await screen.findAllByRole("listitem")
-    expect(firstQuote.textContent).toBe("Gah, stupid sexy Flanders!")
-    expect(secondQuote.textContent).toBe("Eat my shorts")
-    expect(thirdQuote.textContent).toBe("Shut up, brain. I got friends now. I don't need you anymore")
+    const [firstQuote, secondQuote, thirdQuote] = await screen.findAllByRole(
+        "listitem"
+    )
+
+    const [fakeOne, fakeTwo, fakeThird] = fakeQuotes
+    expect(firstQuote.textContent).toBe(fakeOne.quote)
+    expect(secondQuote.textContent).toBe(fakeTwo.quote)
+    expect(thirdQuote.textContent).toBe(fakeThird.quote)
 })
--- assets/prueba/src/components/main-page.js
+++ assets/prueba/src/components/main-page.js
@@ -1,13 +1,29 @@
-const MainPage = ({ React }) => {
+import React from "react"; //Comentar para q funcione en Localhost
+
+const fakeQuotes = [
+    { quote: "Gah, stupid sexy Flanders!" },
+    { quote: "Eat my shorts" },
+    { quote: "Shut up, brain. I got friends now. I don't need you anymore" },
+];
+
+
+// const MainPage = ({ React }) => { //Descomentar para q funcione en localhost
+const MainPage = () => { // Descomentar para q funcione en test
+    React.useEffect(() => {
+        () => fetch("/quotes")
+            .then(response => response.json())
+            .then(data => console.log(data))
+    }, [])
     return (
-        <>
-            <h1 role='heading'>simpsons quote</h1>
+        <div>
+            <h1>simpsons quote</h1>
+
             <ul>
-                <li>Gah, stupid sexy Flanders!</li>
-                <li>Eat my shorts</li>
-                <li>Shut up, brain. I got friends now. I don't need you anymore</li>
+                {fakeQuotes.map(({ quote }) => (
+                    <li key={quote}>{quote}</li>
+                ))}
             </ul>
-        </>
+        </div>
     )
 }
 
