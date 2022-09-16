c82925b2e41d0aa357e6d15e146c473df82c8958
Maximiliano Cabrera <maximilianocabrerap@gmail.com>
2022-09-16 13:12:08 -0300

    AActualizo test y valido cada elemento del array

--- assets/prueba/src/components/main-page.js
+++ assets/prueba/src/components/main-page.js
@@ -3,9 +3,9 @@ const MainPage = ({ React }) => {
         <>
             <h1 role='heading'>simpsons quote</h1>
             <ul>
-                <li>Test</li>
-                <li>Test</li>
-                <li>Test</li>
+                <li>Gah, stupid sexy Flanders!</li>
+                <li>Eat my shorts</li>
+                <li>Shut up, brain. I got friends now. I don't need you anymore</li>
             </ul>
         </>
     )
