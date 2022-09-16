6a906dc75c50774fca230d7dfdfa6ccc5e6179c1
Maximiliano Cabrera <maximilianocabrerap@gmail.com>
2022-09-16 17:03:14 -0300

    Revert "fix browser"
    This reverts commit 15b86b06ec3123d384c56755310ac92b154c1cda.

--- assets/prueba/src/components/main-page.js
+++ assets/prueba/src/components/main-page.js
@@ -1,13 +1,13 @@
 const MainPage = ({ React }) => {
     return (
-        <div>
-            <h1>simpsons quote</h1>
+        <>
+            <h1 role='heading'>simpsons quote</h1>
             <ul>
                 <li>Gah, stupid sexy Flanders!</li>
                 <li>Eat my shorts</li>
                 <li>Shut up, brain. I got friends now. I don't need you anymore</li>
             </ul>
-        </div>
+        </>
     )
 }
 
